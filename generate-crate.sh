8b3960cb3d385baffe6590b04b46ee9e0095c6b3
Corey Farwell <coreyf@rwell.org>
2023-02-21 20:42:21 -0500

    cargo fmt crate

--- generate-crate.sh
+++ generate-crate.sh
@@ -4,4 +4,5 @@ curl https://api.inaturalist.org/v1/swagger.json > swagger.json
 rm -rf crate
 openapi-generator generate -i swagger.json -g rust -o ./crate -c config.json
 rm swagger.json
+(cd crate && cargo fmt)
 
