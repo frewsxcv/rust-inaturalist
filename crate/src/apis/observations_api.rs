a1a5be8cf02d6366b9193cdccfea94969bdd534b
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2025-09-16 17:03:29 -0700

    feat: support JWT auth for GET /observations

--- crate/src/apis/observations_api.rs
+++ crate/src/apis/observations_api.rs
@@ -2362,6 +2362,14 @@ pub async fn observations_get(
     if let Some(ref user_agent) = configuration.user_agent {
         req_builder = req_builder.header(reqwest::header::USER_AGENT, user_agent.clone());
     }
+    if let Some(ref apikey) = configuration.api_key {
+        let key = apikey.key.clone();
+        let value = match apikey.prefix {
+            Some(ref prefix) => format!("{} {}", prefix, key),
+            None => key,
+        };
+        req_builder = req_builder.header("Authorization", value);
+    };
 
     let req = req_builder.build()?;
     let resp = configuration.client.execute(req).await?;
